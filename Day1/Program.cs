e13da11080346566430a41e603f9fdd1a3010082
ENFernandes <elder.fernandes@outlook.pt>
2023-12-05 01:13:56 +0000

    First - Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,28 @@
+﻿var array = File.ReadAllText("C:\\day1_AdventOfCode.txt").Split('\n').ToList();
+var total = 0;
+var isText = "";
+
+array.ForEach((line) =>
+{
+    foreach (var charater in line)
+    {
+        if (!char.IsDigit(charater)) continue;
+        var isNumber = char.IsDigit(charater);
+        if (isNumber)
+        {
+            isText = isText + charater;
+        }
+    }
+    if (isText.Length > 2)
+    {
+        isText = isText[0] + isText[^1].ToString();
+    }
+
+    if (isText.Length < 2)
+        isText = isText + isText;
+
+    total = int.Parse(isText) + total;
+    isText = "";
+});
+
+Console.WriteLine("Calibration is "+total);
